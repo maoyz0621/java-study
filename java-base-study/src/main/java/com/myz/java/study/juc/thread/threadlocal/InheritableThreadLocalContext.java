192e68e19dee948fb395cb7e2ef6747229f81cfd
maoyz <245890416@qq.com>
2020-01-17 15:59:13 +0800

    feat: ThradLocalå±æ§å¯ç»§æ¿æ§ InheritableThreadLocal And ThreadLocal

--- java-base-study/src/main/java/com/myz/java/study/juc/thread/threadlocal/InheritableThreadLocalContext.java
+++ java-base-study/src/main/java/com/myz/java/study/juc/thread/threadlocal/InheritableThreadLocalContext.java
@@ -7,15 +7,50 @@ public class InheritableThreadLocalContext {
 
     private static ThreadLocal<Context> threadLocalContext = new ThreadLocal<>();
 
+    /**
+     * 可继承父线程属性的ThreadLocal
+     */
+    private static ThreadLocal<Context> inheritableThreadLocalContext = new InheritableThreadLocal<>();
+
     static class Context {
 
         String username;
 
         Integer age;
+
+        public String getUsername() {
+            return username;
+        }
+
+        public void setUsername(String username) {
+            this.username = username;
+        }
+
+        public Integer getAge() {
+            return age;
+        }
+
+        public void setAge(Integer age) {
+            this.age = age;
+        }
     }
 
+    public static ThreadLocal<Context> getThreadLocalContext() {
+        return threadLocalContext;
+    }
+
+
+    public static ThreadLocal<Context> getInheritableThreadLocalContext() {
+        return inheritableThreadLocalContext;
+    }
+
+
     public static String get() {
-        return threadLocalContext.get().username;
+        return threadLocalContext.get().username + " => " + threadLocalContext.get().age;
+    }
+
+    public static String get0() {
+        return inheritableThreadLocalContext.get().username + " => " + inheritableThreadLocalContext.get().age;
     }
 
 }
--- java-base-study/src/main/java/com/myz/java/study/juc/thread/threadlocal/InheritableThreadLocalTest.java
+++ java-base-study/src/main/java/com/myz/java/study/juc/thread/threadlocal/InheritableThreadLocalTest.java
@@ -6,7 +6,22 @@ package com.myz.java.study.juc.thread.threadlocal;
 public class InheritableThreadLocalTest {
 
     public static void main(String[] args) {
-        InheritableThreadLocalContext context = new InheritableThreadLocalContext();
-        // InheritableThreadLocalContext.Context
+        InheritableThreadLocalContext.Context context = new InheritableThreadLocalContext.Context();
+        context.setUsername("maoyz");
+        context.setAge(12);
+
+        InheritableThreadLocalContext.getThreadLocalContext().set(context);
+        InheritableThreadLocalContext.getInheritableThreadLocalContext().set(context);
+
+        // NPE
+        // new Thread(() -> {
+        //     System.out.println("ThreadLocal: " + InheritableThreadLocalContext.get());
+        // }).start();
+
+
+        new Thread(() -> {
+            System.out.println("InheritableThreadLocal: " + InheritableThreadLocalContext.get0());
+        }).start();
+
     }
 }
