d0f8afb6a77020ae6c63ce4c9990ab1a37b192ba
maoyz <245890416@qq.com>
2020-03-23 16:14:04 +0800

    strategy

---
+++ java-design/src/main/java/com/myz/design/strategy/pay/strategy/PayStrategy.java
@@ -0,0 +1,19 @@
+package com.myz.design.strategy.pay.strategy;
+
+import java.util.Map;
+
+/**
+ * 支付策略路由
+ */
+public interface PayStrategy {
+
+    /**
+     * 调用对应支付平台组装支付请求报文
+     *
+     * @param payType 传入需要的支付方式
+     * @param params  其他额外需要的参数
+     * @return 生成的支付请求
+     */
+    String generatePayParams(PayType payType, Map<String, Object> params);
+
+}
